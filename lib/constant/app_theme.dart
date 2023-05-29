fa9522e0a613a3f602e689fd93e62437da0e0785
gorimatsu32 <68367302+HitoshiMatsuda@users.noreply.github.com>
2023-05-29 16:32:41 +0900

    定数ファイル作成

---
+++ lib/constant/app_theme.dart
@@ -0,0 +1,5 @@
+///UIに関する定数を扱うクラスである
+///* @author Matsuda
+///* @version1.0.0
+
+class AppTheme {}
---
+++ lib/constant/constant.dart
@@ -0,0 +1,21 @@
+///定数を扱うクラスである
+///* @author Matsuda
+///* @version1.0.0
+
+class Constant {
+  // アプリ共通の定数
+  final String appName = 'トレーニング日記';
+
+  // JSONクラスパス
+  final String jsonPath = 'assets/json/';
+  final String jsonFileName = 'training_Database.json';
+  final String jsonDatabase = 'training_Database';
+
+  // ホーム画面
+  // トレーニング記録入力画面
+  // トレーニング記録閲覧画面
+  // トレーニングメニュー編集画面
+  // 身体記録入力画面
+  // 身体記録閲覧画面
+  // 設定画面
+}
